b86a27e06b30ca09f13bd3ed45ec15ae644d34fd
Tristan-clemenceau <36680670+Tristan-clemenceau@users.noreply.github.com>
2018-08-29 23:02:48 +0200

    Add all the things for the application

---
+++ TP4_CLEMENCEAU_Tristan/ArbreWidget/Form1.Designer.vb
@@ -0,0 +1,387 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form remplace la méthode Dispose pour nettoyer la liste des composants.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requise par le Concepteur Windows Form
+    Private components As System.ComponentModel.IContainer
+
+    'REMARQUE : la procédure suivante est requise par le Concepteur Windows Form
+    'Elle peut être modifiée à l'aide du Concepteur Windows Form.  
+    'Ne la modifiez pas à l'aide de l'éditeur de code.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnQuitter = New System.Windows.Forms.Button()
+        Me.btnRejouer = New System.Windows.Forms.Button()
+        Me.gpProposition = New System.Windows.Forms.GroupBox()
+        Me.btnTester = New System.Windows.Forms.Button()
+        Me.txtSaisie = New System.Windows.Forms.TextBox()
+        Me.lblHistorique = New System.Windows.Forms.Label()
+        Me.gpEssais = New System.Windows.Forms.GroupBox()
+        Me.lblResult = New System.Windows.Forms.Label()
+        Me.lblPropositions = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.ListBox2 = New System.Windows.Forms.ListBox()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.lblMAO = New System.Windows.Forms.Label()
+        Me.lblMotADeviner = New System.Windows.Forms.Label()
+        Me.gpBoxColor = New System.Windows.Forms.GroupBox()
+        Me.btnDefautMode = New System.Windows.Forms.Button()
+        Me.btnWhiteTheme = New System.Windows.Forms.Button()
+        Me.btnGamerMode = New System.Windows.Forms.Button()
+        Me.btnDarkTheme = New System.Windows.Forms.Button()
+        Me.GroupBoxInfo = New System.Windows.Forms.GroupBox()
+        Me.progBar = New System.Windows.Forms.ProgressBar()
+        Me.lblDispnbPartie = New System.Windows.Forms.Label()
+        Me.lblDispVic = New System.Windows.Forms.Label()
+        Me.lblNivAct = New System.Windows.Forms.Label()
+        Me.lblDispDef = New System.Windows.Forms.Label()
+        Me.lblResNombrePartie = New System.Windows.Forms.Label()
+        Me.lblResVictoire = New System.Windows.Forms.Label()
+        Me.lblResDefaite = New System.Windows.Forms.Label()
+        Me.lblNivPro = New System.Windows.Forms.Label()
+        Me.gpProposition.SuspendLayout()
+        Me.gpEssais.SuspendLayout()
+        Me.gpBoxColor.SuspendLayout()
+        Me.GroupBoxInfo.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'btnQuitter
+        '
+        Me.btnQuitter.Location = New System.Drawing.Point(591, 528)
+        Me.btnQuitter.Name = "btnQuitter"
+        Me.btnQuitter.Size = New System.Drawing.Size(92, 34)
+        Me.btnQuitter.TabIndex = 0
+        Me.btnQuitter.Text = "Quitter"
+        Me.btnQuitter.UseVisualStyleBackColor = True
+        '
+        'btnRejouer
+        '
+        Me.btnRejouer.Location = New System.Drawing.Point(471, 528)
+        Me.btnRejouer.Name = "btnRejouer"
+        Me.btnRejouer.Size = New System.Drawing.Size(92, 34)
+        Me.btnRejouer.TabIndex = 1
+        Me.btnRejouer.Text = "Rejouer"
+        Me.btnRejouer.UseVisualStyleBackColor = True
+        '
+        'gpProposition
+        '
+        Me.gpProposition.AutoSize = True
+        Me.gpProposition.Controls.Add(Me.btnTester)
+        Me.gpProposition.Controls.Add(Me.txtSaisie)
+        Me.gpProposition.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.25!)
+        Me.gpProposition.Location = New System.Drawing.Point(24, 63)
+        Me.gpProposition.Name = "gpProposition"
+        Me.gpProposition.Size = New System.Drawing.Size(316, 114)
+        Me.gpProposition.TabIndex = 2
+        Me.gpProposition.TabStop = False
+        Me.gpProposition.Text = "Proposition"
+        '
+        'btnTester
+        '
+        Me.btnTester.Location = New System.Drawing.Point(207, 46)
+        Me.btnTester.Name = "btnTester"
+        Me.btnTester.Size = New System.Drawing.Size(92, 34)
+        Me.btnTester.TabIndex = 1
+        Me.btnTester.Text = "Tester"
+        Me.btnTester.UseVisualStyleBackColor = True
+        '
+        'txtSaisie
+        '
+        Me.txtSaisie.Location = New System.Drawing.Point(26, 51)
+        Me.txtSaisie.Name = "txtSaisie"
+        Me.txtSaisie.Size = New System.Drawing.Size(165, 23)
+        Me.txtSaisie.TabIndex = 0
+        '
+        'lblHistorique
+        '
+        Me.lblHistorique.AutoSize = True
+        Me.lblHistorique.Font = New System.Drawing.Font("Microsoft Sans Serif", 25.25!)
+        Me.lblHistorique.Location = New System.Drawing.Point(444, 23)
+        Me.lblHistorique.Name = "lblHistorique"
+        Me.lblHistorique.Size = New System.Drawing.Size(171, 39)
+        Me.lblHistorique.TabIndex = 3
+        Me.lblHistorique.Text = "Historique"
+        '
+        'gpEssais
+        '
+        Me.gpEssais.Controls.Add(Me.lblResult)
+        Me.gpEssais.Controls.Add(Me.lblPropositions)
+        Me.gpEssais.Controls.Add(Me.Label1)
+        Me.gpEssais.Controls.Add(Me.ListBox2)
+        Me.gpEssais.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.25!)
+        Me.gpEssais.Location = New System.Drawing.Point(24, 284)
+        Me.gpEssais.Name = "gpEssais"
+        Me.gpEssais.Size = New System.Drawing.Size(316, 273)
+        Me.gpEssais.TabIndex = 4
+        Me.gpEssais.TabStop = False
+        Me.gpEssais.Text = "Essais"
+        '
+        'lblResult
+        '
+        Me.lblResult.Location = New System.Drawing.Point(97, 23)
+        Me.lblResult.Name = "lblResult"
+        Me.lblResult.Size = New System.Drawing.Size(41, 26)
+        Me.lblResult.TabIndex = 3
+        '
+        'lblPropositions
+        '
+        Me.lblPropositions.AutoSize = True
+        Me.lblPropositions.Location = New System.Drawing.Point(29, 61)
+        Me.lblPropositions.Name = "lblPropositions"
+        Me.lblPropositions.Size = New System.Drawing.Size(94, 17)
+        Me.lblPropositions.TabIndex = 2
+        Me.lblPropositions.Text = "Propositions :"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(26, 23)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(65, 17)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Restant :"
+        '
+        'ListBox2
+        '
+        Me.ListBox2.FormattingEnabled = True
+        Me.ListBox2.ItemHeight = 17
+        Me.ListBox2.Location = New System.Drawing.Point(26, 103)
+        Me.ListBox2.Name = "ListBox2"
+        Me.ListBox2.Size = New System.Drawing.Size(242, 140)
+        Me.ListBox2.TabIndex = 0
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.Location = New System.Drawing.Point(428, 63)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(233, 290)
+        Me.ListBox1.TabIndex = 5
+        '
+        'lblMAO
+        '
+        Me.lblMAO.AutoSize = True
+        Me.lblMAO.Location = New System.Drawing.Point(24, 13)
+        Me.lblMAO.Name = "lblMAO"
+        Me.lblMAO.Size = New System.Drawing.Size(81, 13)
+        Me.lblMAO.TabIndex = 6
+        Me.lblMAO.Text = "Mot à Ordonner"
+        '
+        'lblMotADeviner
+        '
+        Me.lblMotADeviner.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.25!)
+        Me.lblMotADeviner.Location = New System.Drawing.Point(53, 30)
+        Me.lblMotADeviner.Name = "lblMotADeviner"
+        Me.lblMotADeviner.Size = New System.Drawing.Size(100, 23)
+        Me.lblMotADeviner.TabIndex = 7
+        '
+        'gpBoxColor
+        '
+        Me.gpBoxColor.Controls.Add(Me.btnDefautMode)
+        Me.gpBoxColor.Controls.Add(Me.btnWhiteTheme)
+        Me.gpBoxColor.Controls.Add(Me.btnGamerMode)
+        Me.gpBoxColor.Controls.Add(Me.btnDarkTheme)
+        Me.gpBoxColor.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.25!)
+        Me.gpBoxColor.Location = New System.Drawing.Point(368, 368)
+        Me.gpBoxColor.Name = "gpBoxColor"
+        Me.gpBoxColor.Size = New System.Drawing.Size(293, 140)
+        Me.gpBoxColor.TabIndex = 8
+        Me.gpBoxColor.TabStop = False
+        Me.gpBoxColor.Text = "Mode"
+        '
+        'btnDefautMode
+        '
+        Me.btnDefautMode.Location = New System.Drawing.Point(163, 87)
+        Me.btnDefautMode.Name = "btnDefautMode"
+        Me.btnDefautMode.Size = New System.Drawing.Size(106, 31)
+        Me.btnDefautMode.TabIndex = 3
+        Me.btnDefautMode.Text = "Defaut Theme"
+        Me.btnDefautMode.UseVisualStyleBackColor = True
+        '
+        'btnWhiteTheme
+        '
+        Me.btnWhiteTheme.Location = New System.Drawing.Point(163, 30)
+        Me.btnWhiteTheme.Name = "btnWhiteTheme"
+        Me.btnWhiteTheme.Size = New System.Drawing.Size(106, 31)
+        Me.btnWhiteTheme.TabIndex = 2
+        Me.btnWhiteTheme.Text = "White Theme"
+        Me.btnWhiteTheme.UseVisualStyleBackColor = True
+        '
+        'btnGamerMode
+        '
+        Me.btnGamerMode.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.25!)
+        Me.btnGamerMode.Location = New System.Drawing.Point(19, 87)
+        Me.btnGamerMode.Name = "btnGamerMode"
+        Me.btnGamerMode.Size = New System.Drawing.Size(106, 31)
+        Me.btnGamerMode.TabIndex = 1
+        Me.btnGamerMode.Text = "Gamer Theme"
+        Me.btnGamerMode.UseVisualStyleBackColor = True
+        '
+        'btnDarkTheme
+        '
+        Me.btnDarkTheme.Location = New System.Drawing.Point(19, 30)
+        Me.btnDarkTheme.Name = "btnDarkTheme"
+        Me.btnDarkTheme.Size = New System.Drawing.Size(106, 31)
+        Me.btnDarkTheme.TabIndex = 0
+        Me.btnDarkTheme.Text = "Dark Theme"
+        Me.btnDarkTheme.UseVisualStyleBackColor = True
+        '
+        'GroupBoxInfo
+        '
+        Me.GroupBoxInfo.Controls.Add(Me.lblNivPro)
+        Me.GroupBoxInfo.Controls.Add(Me.lblResDefaite)
+        Me.GroupBoxInfo.Controls.Add(Me.lblResVictoire)
+        Me.GroupBoxInfo.Controls.Add(Me.lblResNombrePartie)
+        Me.GroupBoxInfo.Controls.Add(Me.lblDispDef)
+        Me.GroupBoxInfo.Controls.Add(Me.lblNivAct)
+        Me.GroupBoxInfo.Controls.Add(Me.lblDispVic)
+        Me.GroupBoxInfo.Controls.Add(Me.lblDispnbPartie)
+        Me.GroupBoxInfo.Controls.Add(Me.progBar)
+        Me.GroupBoxInfo.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.25!)
+        Me.GroupBoxInfo.Location = New System.Drawing.Point(24, 183)
+        Me.GroupBoxInfo.Name = "GroupBoxInfo"
+        Me.GroupBoxInfo.Size = New System.Drawing.Size(316, 95)
+        Me.GroupBoxInfo.TabIndex = 9
+        Me.GroupBoxInfo.TabStop = False
+        Me.GroupBoxInfo.Text = "Info"
+        '
+        'progBar
+        '
+        Me.progBar.Location = New System.Drawing.Point(82, 75)
+        Me.progBar.Name = "progBar"
+        Me.progBar.Size = New System.Drawing.Size(125, 14)
+        Me.progBar.TabIndex = 4
+        '
+        'lblDispnbPartie
+        '
+        Me.lblDispnbPartie.AutoSize = True
+        Me.lblDispnbPartie.Location = New System.Drawing.Point(6, 19)
+        Me.lblDispnbPartie.Name = "lblDispnbPartie"
+        Me.lblDispnbPartie.Size = New System.Drawing.Size(107, 17)
+        Me.lblDispnbPartie.TabIndex = 5
+        Me.lblDispnbPartie.Text = "Nombre Partie :"
+        '
+        'lblDispVic
+        '
+        Me.lblDispVic.AutoSize = True
+        Me.lblDispVic.Location = New System.Drawing.Point(30, 36)
+        Me.lblDispVic.Name = "lblDispVic"
+        Me.lblDispVic.Size = New System.Drawing.Size(117, 17)
+        Me.lblDispVic.TabIndex = 6
+        Me.lblDispVic.Text = "Nombre Victoire :"
+        '
+        'lblNivAct
+        '
+        Me.lblNivAct.Location = New System.Drawing.Point(30, 72)
+        Me.lblNivAct.Name = "lblNivAct"
+        Me.lblNivAct.Size = New System.Drawing.Size(48, 17)
+        Me.lblNivAct.TabIndex = 7
+        '
+        'lblDispDef
+        '
+        Me.lblDispDef.AutoSize = True
+        Me.lblDispDef.Location = New System.Drawing.Point(30, 53)
+        Me.lblDispDef.Name = "lblDispDef"
+        Me.lblDispDef.Size = New System.Drawing.Size(115, 17)
+        Me.lblDispDef.TabIndex = 9
+        Me.lblDispDef.Text = "Nombre Defaite :"
+        '
+        'lblResNombrePartie
+        '
+        Me.lblResNombrePartie.Location = New System.Drawing.Point(108, 19)
+        Me.lblResNombrePartie.Name = "lblResNombrePartie"
+        Me.lblResNombrePartie.Size = New System.Drawing.Size(39, 17)
+        Me.lblResNombrePartie.TabIndex = 10
+        '
+        'lblResVictoire
+        '
+        Me.lblResVictoire.Location = New System.Drawing.Point(153, 36)
+        Me.lblResVictoire.Name = "lblResVictoire"
+        Me.lblResVictoire.Size = New System.Drawing.Size(39, 17)
+        Me.lblResVictoire.TabIndex = 11
+        '
+        'lblResDefaite
+        '
+        Me.lblResDefaite.Location = New System.Drawing.Point(153, 55)
+        Me.lblResDefaite.Name = "lblResDefaite"
+        Me.lblResDefaite.Size = New System.Drawing.Size(39, 17)
+        Me.lblResDefaite.TabIndex = 12
+        '
+        'lblNivPro
+        '
+        Me.lblNivPro.Location = New System.Drawing.Point(217, 72)
+        Me.lblNivPro.Name = "lblNivPro"
+        Me.lblNivPro.Size = New System.Drawing.Size(58, 17)
+        Me.lblNivPro.TabIndex = 13
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(704, 586)
+        Me.Controls.Add(Me.GroupBoxInfo)
+        Me.Controls.Add(Me.gpBoxColor)
+        Me.Controls.Add(Me.lblMotADeviner)
+        Me.Controls.Add(Me.lblMAO)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Controls.Add(Me.gpEssais)
+        Me.Controls.Add(Me.lblHistorique)
+        Me.Controls.Add(Me.gpProposition)
+        Me.Controls.Add(Me.btnRejouer)
+        Me.Controls.Add(Me.btnQuitter)
+        Me.Name = "Form1"
+        Me.Text = "Jeu De Mots"
+        Me.gpProposition.ResumeLayout(False)
+        Me.gpProposition.PerformLayout()
+        Me.gpEssais.ResumeLayout(False)
+        Me.gpEssais.PerformLayout()
+        Me.gpBoxColor.ResumeLayout(False)
+        Me.GroupBoxInfo.ResumeLayout(False)
+        Me.GroupBoxInfo.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btnQuitter As Button
+    Friend WithEvents btnRejouer As Button
+    Friend WithEvents gpProposition As GroupBox
+    Friend WithEvents lblHistorique As Label
+    Friend WithEvents btnTester As Button
+    Friend WithEvents txtSaisie As TextBox
+    Friend WithEvents gpEssais As GroupBox
+    Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents lblPropositions As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents ListBox2 As ListBox
+    Friend WithEvents lblResult As Label
+    Friend WithEvents lblMAO As Label
+    Friend WithEvents lblMotADeviner As Label
+    Friend WithEvents gpBoxColor As GroupBox
+    Friend WithEvents btnWhiteTheme As Button
+    Friend WithEvents btnGamerMode As Button
+    Friend WithEvents btnDarkTheme As Button
+    Friend WithEvents btnDefautMode As Button
+    Friend WithEvents progBar As ProgressBar
+    Friend WithEvents GroupBoxInfo As GroupBox
+    Friend WithEvents lblNivAct As Label
+    Friend WithEvents lblDispVic As Label
+    Friend WithEvents lblDispnbPartie As Label
+    Friend WithEvents lblDispDef As Label
+    Friend WithEvents lblResNombrePartie As Label
+    Friend WithEvents lblResDefaite As Label
+    Friend WithEvents lblResVictoire As Label
+    Friend WithEvents lblNivPro As Label
+End Class
---
+++ TP4_CLEMENCEAU_Tristan/ArbreWidget/Form1.vb
@@ -0,0 +1,243 @@
+﻿Public Class Form1
+
+    Private tabMot() As String 'Declaration tab string'
+    Private Essais As Integer = 8
+    Private ValAlea As Integer
+    Private NbMotTrouve As Integer
+    Private NbPartie As Integer = 1
+    Private nbCoupPartie As Integer = 0
+    Private PartieEtat As Boolean = False
+    Private nbVictoire As Integer = 0
+    Private nbDefaite As Integer = 0
+    Private niveau As Integer = 0
+    Private xpPlayer As Integer = 0
+    Private Const VICT As Integer = 50
+    Private Const DEFAITE As Integer = 20
+
+
+    Private Sub Initialisation()
+
+        ColorUpdate(212, 101, 232, 51, 55, 69, 119, 196, 211, 225, 237, 100, 234, 46, 73)
+        tabMot = {"FOOO", "BAR", "AN", "OTHER", "STRING"}
+        lblResult.Text = Essais
+        xpPlayer = 10
+        ListBox1.Items.Add("Bienvenue Profil + " + CStr(xpPlayer) + " XP")
+        progBar.Value = xpPlayer
+
+        Dim rand As New Random()
+
+        ValAlea = rand.Next(0, (tabMot.Length - 1))
+        Melanger(tabMot(ValAlea))
+
+        lblResNombrePartie.Text = NbPartie
+        lblResDefaite.Text = nbDefaite
+        lblResVictoire.Text = nbVictoire
+        lblNivAct.Text = "LVL" + CStr(niveau)
+        lblNivPro.Text = "LVL" + CStr(niveau + 1)
+
+    End Sub
+
+    Private Function Melanger(ByVal chaine As String) As String
+        Dim rand As New Random()
+        Dim newchaine = ""
+        For cpt As Integer = 0 To (chaine.Length - 1)
+            newchaine = newchaine.Insert(rand.Next(0, newchaine.Length), chaine(cpt))
+        Next
+        lblMotADeviner.Text = newchaine
+        Return newchaine
+    End Function
+
+    Private Sub NouvellePartie()
+
+        lblMotADeviner.Text = ""
+        Dim rand As New Random()
+        ValAlea = rand.Next(0, (tabMot.Length - 1))
+        lblMotADeviner.Text = Melanger(tabMot(ValAlea))
+        Historique()
+        MiseAjour()
+        NbPartie = NbPartie + 1
+        ListBox2.Items.Clear()
+        nbCoupPartie = 0
+        txtSaisie.Text = ""
+        Essais = 8
+        lblResult.Text = Essais
+        PartieEtat = False
+
+    End Sub
+
+
+    Private Sub MotCorrect()
+        nbCoupPartie = nbCoupPartie + 1
+        If (txtSaisie.Text.ToUpper Like tabMot(ValAlea)) Then
+            PartieEtat = True
+            NbMotTrouve = NbMotTrouve + 1
+            xpPlayer = xpPlayer + VICT
+            If MessageBox.Show("Voulez-vous rejouez ?", "Partie Gagnée", MessageBoxButtons.YesNo, MessageBoxIcon.Question) = vbYes Then
+                NouvellePartie()
+            End If
+        Else
+            ListBox2.Items.Add(txtSaisie.Text)
+            Essais = Essais - 1
+            MotInorrect()
+        End If
+    End Sub
+
+    Private Sub MotInorrect()
+        MiseAjour()
+        If (Essais = 0) Then
+            xpPlayer = xpPlayer + DEFAITE
+            If MessageBox.Show("Tu as perdu , le mot etait [" + tabMot(ValAlea) + "] , Voulez vous rejouer ?", "Partie PERDU", MessageBoxButtons.YesNo, MessageBoxIcon.Question) = vbYes Then
+                lblMotADeviner.Text = ""
+                NouvellePartie()
+            End If
+        End If
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Initialisation()
+    End Sub
+
+    Private Sub btnRejouer_Click(sender As Object, e As EventArgs) Handles btnRejouer.Click
+        If lblMotADeviner.Text = "" Then
+            NouvellePartie()
+        Else
+            MessageBox.Show("Tu ne peux pas relancer une nouvelle partie ", "Erreur", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End If
+    End Sub
+
+    Private Sub NiveauUpdate()
+        If xpPlayer >= 100 Then
+            niveau = niveau + 1
+            ListBox1.Items.Add("Vous venez de passer LVL : " + CStr(niveau))
+            xpPlayer = 0
+        End If
+    End Sub
+
+    Private Sub MiseAjour()
+
+        lblResDefaite.Text = nbDefaite
+        lblResVictoire.Text = nbVictoire
+        lblNivAct.Text = "LVL" + CStr(niveau)
+        lblNivPro.Text = "LVL" + CStr(niveau + 1)
+        lblResNombrePartie.Text = NbPartie
+        lblResult.Text = Essais
+        NiveauUpdate()
+        progBar.Value = xpPlayer
+        txtSaisie.Clear()
+    End Sub
+
+    Private Sub ColorUpdate(R01 As Integer, G01 As Integer, B01 As Integer, R02 As Integer, G02 As Integer, B02 As Integer, R03 As Integer, G03 As Integer, B03 As Integer, R04 As Integer, G04 As Integer, B04 As Integer, R05 As Integer, G05 As Integer, B05 As Integer) 'Permet de Changer la couleur des elements presents dans la fenetres'
+        'RGB 1="BackGround application" RGB 2=Background Label RGB 3=Color Police RGB 4=GroupeBox RGB 5=Bouton Color'
+        Me.BackColor = Color.FromArgb(R01, G01, B01)
+        btnTester.ForeColor = Color.FromArgb(R01, G01, B01)
+        btnDarkTheme.ForeColor = Color.FromArgb(R01, G01, B01)
+        btnDefautMode.ForeColor = Color.FromArgb(R01, G01, B01)
+        btnGamerMode.ForeColor = Color.FromArgb(R01, G01, B01)
+        btnWhiteTheme.ForeColor = Color.FromArgb(R01, G01, B01)
+        btnQuitter.ForeColor = Color.FromArgb(R01, G01, B01)
+        btnRejouer.ForeColor = Color.FromArgb(R01, G01, B01)
+
+        lblHistorique.BackColor = Color.FromArgb(R02, G02, B02)
+        lblMAO.BackColor = Color.FromArgb(R02, G02, B02)
+        lblMotADeviner.BackColor = Color.FromArgb(R02, G02, B02)
+        lblPropositions.BackColor = Color.FromArgb(R02, G02, B02)
+        lblResult.BackColor = Color.FromArgb(R02, G02, B02)
+        gpEssais.BackColor = Color.FromArgb(R02, G02, B02)
+        gpProposition.BackColor = Color.FromArgb(R02, G02, B02)
+        gpBoxColor.BackColor = Color.FromArgb(R02, G02, B02)
+        GroupBoxInfo.BackColor = Color.FromArgb(R02, G02, B02)
+        ListBox2.ForeColor = Color.FromArgb(R02, G02, B02)
+        ListBox1.ForeColor = Color.FromArgb(R02, G02, B02)
+        txtSaisie.ForeColor = Color.FromArgb(R02, G02, B02)
+
+        lblHistorique.ForeColor = Color.FromArgb(R03, G03, B03)
+        lblMAO.ForeColor = Color.FromArgb(R03, G03, B03)
+        lblMotADeviner.ForeColor = Color.FromArgb(R03, G03, B03)
+        lblPropositions.ForeColor = Color.FromArgb(R03, G03, B03)
+        lblResult.ForeColor = Color.FromArgb(R03, G03, B03)
+        GroupBoxInfo.ForeColor = Color.FromArgb(R03, G03, B03)
+        gpEssais.ForeColor = Color.FromArgb(R03, G03, B03)
+        gpProposition.ForeColor = Color.FromArgb(R03, G03, B03)
+        gpBoxColor.ForeColor = Color.FromArgb(R03, G03, B03)
+        lblDispnbPartie.ForeColor = Color.FromArgb(R03, G03, B03)
+        lblResNombrePartie.ForeColor = Color.FromArgb(R03, G03, B03)
+        lblDispDef.ForeColor = Color.FromArgb(R03, G03, B03)
+        lblDispVic.ForeColor = Color.FromArgb(R03, G03, B03)
+        lblResVictoire.ForeColor = Color.FromArgb(R03, G03, B03)
+        lblResDefaite.ForeColor = Color.FromArgb(R03, G03, B03)
+        lblNivPro.ForeColor = Color.FromArgb(R03, G03, B03)
+        lblNivAct.ForeColor = Color.FromArgb(R03, G03, B03)
+
+
+        btnTester.BackColor = Color.FromArgb(R04, G04, B04)
+        btnDefautMode.BackColor = Color.FromArgb(R04, G04, B04)
+        btnDarkTheme.BackColor = Color.FromArgb(R04, G04, B04)
+        btnGamerMode.BackColor = Color.FromArgb(R04, G04, B04)
+        btnWhiteTheme.BackColor = Color.FromArgb(R04, G04, B04)
+        btnQuitter.BackColor = Color.FromArgb(R04, G04, B04)
+        btnRejouer.BackColor = Color.FromArgb(R04, G04, B04)
+
+
+        'GRIS'
+        txtSaisie.BackColor = Color.FromArgb(R05, G05, B05)
+        ListBox2.BackColor = Color.FromArgb(R05, G05, B05)
+        ListBox1.BackColor = Color.FromArgb(R05, G05, B05)
+
+
+
+
+    End Sub
+
+    Private Sub Historique()
+        If PartieEtat = True Then
+            nbVictoire = nbVictoire + 1
+            ListBox1.Items.Add("Partie" + CStr(NbPartie) + " : Gagné " + " [" + CStr(nbCoupPartie) + " Essai(s)]")
+        Else
+            nbDefaite = nbDefaite + 1
+            ListBox1.Items.Add("Partie" + CStr(NbPartie) + " : Perdu")
+        End If
+    End Sub
+
+    Private Sub btnTester_Click(sender As Object, e As EventArgs) Handles btnTester.Click
+        If txtSaisie.Text = "" Then
+        Else
+            MotCorrect()
+            MotInorrect()
+        End If
+    End Sub
+
+    Private Sub btnQuitter_Click(sender As Object, e As EventArgs) Handles btnQuitter.Click
+
+        If MessageBox.Show(CStr(NbMotTrouve) + " mot(s) trouvée sur " + CStr(NbPartie) + " partie(s)", "Quitter", MessageBoxButtons.OK, MessageBoxIcon.Question) = vbOK Then
+            Me.Dispose()
+        End If
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles btnDarkTheme.Click 'Dark theme https://color.adobe.com/fr/ATLANTHYS-TOOL-color-theme-8090318/edit/?copy=true'
+        ColorUpdate(1, 0, 0, 92, 0, 9, 189, 183, 184, 148, 3, 21, 57, 56, 69)
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles btnWhiteTheme.Click 'White theme https://color.adobe.com/fr/ad4fce445f05340350b92478ebeaa1f6-color-theme-10551296/edit/?copy=true'
+        ColorUpdate(73, 49, 168, 113, 109, 249, 117, 226, 228, 128, 126, 251, 72, 205, 210)
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles btnGamerMode.Click 'Gamer Mode https://color.adobe.com/fr/CS04-color-theme-1994456/edit/?copy=true'
+        ColorUpdate(51, 55, 69, 218, 237, 226, 51, 55, 69, 119, 196, 211, 234, 46, 73)
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles btnDefautMode.Click 'Default Mode'
+        ColorUpdate(212, 101, 232, 51, 55, 69, 119, 196, 211, 225, 237, 100, 234, 46, 73)
+    End Sub
+
+    Private Sub ProgressBar1_Click(sender As Object, e As EventArgs) Handles progBar.Click
+
+    End Sub
+
+    Private Sub Label5_Click(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub Label2_Click(sender As Object, e As EventArgs) Handles lblResNombrePartie.Click
+
+    End Sub
+End Class
